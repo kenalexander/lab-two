fd6a32f9d27e112507eefca3e20908c6d0047e30
kenalexander <kenoliveralexander@gmail.com>
2018-07-22 19:40:07 -0400

    here

---
+++ labtwo/config/connect.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // db type/server/database
+  // db: 'mongodb://dbusername:dbpassword@ds153890.mlab.com:53890/comp2068'
+  db: 'mongodb://koalexander:wireless101@ds261440.mlab.com:61440/comp2068'
+}
--- labtwo/routes/index.js
+++ labtwo/routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ken, The Second Lab' });
+  res.render('index', { 
+  	title: 'Ken, The Second Lab' });
+});
+
+router.get('/views/ken1', function(req, res) {
+  res.render('ken1', { 
+  	title: 'Ken, First of His Name' });
+});
+router.get('/views/ken2', function(req, res) {
+  res.render('ken2', { 
+  	title: 'Ken, II' });
+});
+router.get('/views/ken3', function(req, res) {
+  res.render('ken3', { 
+  	title: 'Ken, III' });
 });
 
 module.exports = router;
